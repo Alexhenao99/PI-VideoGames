bd5bc467991698b34a8142a499ca864c5898a797
Alexanderhenao99 <alexanderhenao0522@gmail.com>
2023-02-11 16:09:27 -0500

    Videogame-creationModel

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -1,12 +1,39 @@
 const { DataTypes } = require('sequelize');
+
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
-module.exports = (sequelize) => {
+module.exports = ( sequelize ) => {
   // defino el modelo
-  sequelize.define('videogame', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    'Videogames', 
+    {
+      id:{
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        defaultValue: 'https://i.blogs.es/7796de/portada-xbox/840_560.jpeg'
+      },
+      releaseDate: {
+        type: DataTypes.DATEONLY,
+        allowNull: false,
+      },
+      rating: {
+        type: DataTypes.ENUM( 'Bad', 'Good', 'Excellent' )
+      },
     },
-  });
+    {
+      timestamps: false
+    }
+  );
 };
