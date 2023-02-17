d468f6f14b5c23948da996dbd3ef16b18f8b59ec
Alexanderhenao99 <alexanderhenao0522@gmail.com>
2023-02-16 23:02:00 -0500

    Update configurations and css

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import { Route, useLocation } from "react-router-dom" 
+import Landing from "./pages/Landing/Landing";
+import Home from './pages/Home/Home';
+import CreateGame from './pages/Form/CreateGame';
+import Detail from './pages/Detail/Detail';
+import Nav from './pages/Navbar/Nav';
+import Footer from './pages/Footer/Footer';
 
 function App() {
+  const location = useLocation();
   return (
     <div className="App">
-      <h1>Henry Videogames</h1>
+      {location.pathname !== '/' && <Nav/>} 
+      <Route exact path={'/'} component={ Landing } />
+      <Route exact path={'/home'} component={ Home } />
+      <Route exact path={'/creategame'} component={ CreateGame } />
+      <Route exact path={'/detail/:id'} component={ Detail } />
+      {location.pathname !== '/' && <Footer/>} 
     </div>
   );
 }
