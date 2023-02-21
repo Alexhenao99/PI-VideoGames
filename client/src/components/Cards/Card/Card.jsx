57519eef45affc2b702ef59cc312f8f9a00fffcd
Alexanderhenao99 <alexanderhenao0522@gmail.com>
2023-02-21 00:57:47 -0500

    Correction key

--- client/src/components/Cards/Card/Card.jsx
+++ client/src/components/Cards/Card/Card.jsx
@@ -8,24 +8,24 @@ import android from '../../../Images/Platforms/android.png';
 import './Card.css';
 
 function Card({ games }) {
-    const { name, image, parent_platforms, genres } = games;
+    const { name, image, rating, parent_platforms, genres } = games;
     
     // muestra el logo de la plataforma
     const platforms = ( platforms ) => {
         return(
             // eslint-disable-next-line array-callback-return
-            platforms.map( ( platform ) => {
+            platforms.map( ( platform, i ) => {
                 switch (platform.toLowerCase()) {
                     case 'xbox':
-                        return <img src={xbox} alt="Logo Xbox" />
+                        return <img src={ xbox } alt="Logo Xbox" key={ i }/>
                     case 'playstation':
-                        return <img src={ playstation } alt="Logo Playstation" />
+                        return <img src={ playstation } alt="Logo Playstation" key={ i }/>
                     case 'nintendo':
-                        return <img src={ nintendo } alt="Logo Nintendo" />
+                        return <img src={ nintendo } alt="Logo Nintendo" key={ i }/>
                     case 'pc':
-                        return <img src={ pc } alt="Logo Pc" />
+                        return <img src={ pc } alt="Logo Pc" key={ i }/>
                     case 'android':
-                        return <img src={ android } alt="Logo Android" />
+                        return <img src={ android } alt="Logo Android" key={ i }/>
                     default:
                     break;
                 }
@@ -34,7 +34,7 @@ function Card({ games }) {
     }
     
     return(
-        <div id='card'>
+        <div className='card'>
             <div className='imgCard'>
                 <img className='img' src={ image } alt={ name } />
             </div>
@@ -44,6 +44,7 @@ function Card({ games }) {
                         { platforms( parent_platforms ) }
                     </section>
                     <h1 className='name'> { name } </h1>
+                    <h1 className='rating'> { rating } </h1>
                 </section>
                 <section className='cardOpen'>
                     <p className='cardGenres'> 
