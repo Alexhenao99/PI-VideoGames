8bfa946710ea519989078fc8cff5074eb3524674
Alexanderhenao99 <alexanderhenao0522@gmail.com>
2023-02-22 01:58:49 -0500

    Update - add Searchbar

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import CreateGame from './pages/Form/CreateGame';
 import Detail from './pages/Detail/Detail';
 import Nav from './pages/Navbar/Nav';
 import Footer from './pages/Footer/Footer';
+import Searchbar from './pages/Navbar/Searchbar/Searchbar';
 
 function App() {
   const location = useLocation();
@@ -14,6 +15,7 @@ function App() {
       {location.pathname !== '/' && <Nav/>} 
       <Route exact path={'/'} component={ Landing } />
       <Route exact path={'/home'} component={ Home } />
+      <Route exact path="/results/:name" component={ Searchbar } />
       <Route exact path={'/creategame'} component={ CreateGame } />
       <Route exact path={'/detail/:id'} component={ Detail } />
       {location.pathname !== '/' && <Footer/>} 
--- client/src/pages/Navbar/Nav.jsx
+++ client/src/pages/Navbar/Nav.jsx
@@ -1,22 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import Searchbar from '../../components/Searchbar/Searchbar'
 import logo from '../../Images/Logo.png'
 
 function Nav() {
+    const [ name, setName ] = useState("");
+    const resetValueSearch = () => {
+        document.getElementById("search").value = ""
+    };
     return (
-        <div id="Nav">
+        <nav id="Nav">
             <Link to={ '/home' }>
                 <section className='logo'>
                     <img src={ logo } className='logo' alt="Logo" />
                     <p> Henry Games </p>
                 </section>
             </Link>
-            <Searchbar/>
+            <div className="searchbar">
+                <input
+                    id='search'
+                    onChange={( event ) => setName( event.target.value )}
+                    placeholder="Search videogame..."
+                    type="text"
+                />
+                <Link to={`/results/${name}`}>
+                    <button name="name" type="submit" onClick={ resetValueSearch }> Go! </button>
+                </Link>
+            </div>
             <Link to={ '/creategame' }>
                 <div className="creategame">Add Game</div>
             </Link>
-        </div>
+        </nav>
     )
 }
 
