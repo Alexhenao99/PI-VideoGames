e28e51c06849f09c39cb7e1f06629ebad9b85d04
Alexanderhenao99 <alexanderhenao0522@gmail.com>
2023-02-26 17:42:52 -0500

    Update - game not fount

--- client/src/pages/Detail/Detail.jsx
+++ client/src/pages/Detail/Detail.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom';
-import { getDetails } from '../../redux/actions';
+import { deleteGame, getDetails } from '../../redux/actions';
+import { Link } from 'react-router-dom';
 
 const Detail = () => {
     const { id } = useParams();
@@ -11,27 +12,47 @@ const Detail = () => {
     
     useEffect( () => {
         dispatch( getDetails( id ))
-    },[dispatch, id])
+    },[ dispatch, id ])   
     
     function description( about ) {
         return about.includes('<p>') 
         ? document.getElementById("description").innerHTML = about
         : document.getElementById("description").innerHTML = "<p>"+about+"</p>"
     }
-
+    
     const platforms = ( platforms ) => {
         if( platforms ) return platforms.join(", ")
     }
-
+    
     const genres = ( genres ) => {
         if( genres ) return Object.values( genres.map( genre => genre.name ).join(", "))
     }
     
     if(detail.length !== 0 && detail.id.toString() === id ) description( detail.description ) 
     
+    function handlerDelete(){
+        dispatch( deleteGame( id ) )
+    };
+    function handlerEdit(){
+        return console.log(id)
+    };
     
     return (
         <div id="details">
+            { 
+                detail ? 
+                    <Link to={ "/home" }> <div className="notFound"> Game Not Found </div> </Link> 
+                        : detail.created === true 
+                        ? <div className="buttons">
+                            <Link to={`/updategema/${ id }`}>
+                                <button onClick={ () => handlerEdit() } > Edit </button>
+                            </Link>
+                            <a href="/home">
+                                <button onClick={ handlerDelete } > Delete </button>
+                            </a>
+                        </div>  
+                        : <p style={{display:'none'}}/> 
+            }
             <img src={ detail.image } alt={detail.name} style={{width:"500px"}} />
             <h1 className="name"> {detail.name} </h1>
             <div id="description"></div>
