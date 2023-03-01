d2a221cf571b2ffec6a1014e25c64fc3f0da56b9
Alexanderhenao99 <alexanderhenao0522@gmail.com>
2023-03-01 09:41:20 -0500

    Correction urlsequelize

--- api/src/db.js
+++ api/src/db.js
@@ -4,9 +4,9 @@ const path = require('path');
 
 // .env
 require('dotenv').config();
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
