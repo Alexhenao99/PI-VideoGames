a1f97e2fc0098988c468306f516da9764ea89913
Alexanderhenao99 <alexanderhenao0522@gmail.com>
2023-03-01 09:00:24 -0500

    Update - Code refactoring

--- api/index.js
+++ api/index.js
@@ -19,10 +19,12 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+require('dotenv').config()
+const { PORT } = process.env;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(process.env.PORT, () => {
+    console.log('%s listening at 3001', process.env.PORT); // eslint-disable-line no-console
   });
 });
